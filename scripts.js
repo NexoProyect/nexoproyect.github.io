86520edfde0f04bf2f7c9f3cb8dfec3b3eaccee1
Nexo <sm8contact@gmail.com>
2025-01-09 21:03:35 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 document.getElementById("joinButton").addEventListener("click", function() {
-alert("Gracias por unirte a la comunidad de Nexo. ¡Pronto serás redireccionado al repositorio oficial!");    
-setTimeout(() => {
-window.location.href = "https://github.com/NexoProyect/NexoProyect/blob/main/README.md";
-}, 3000);
+    alert("Gracias por unirte a la comunidad de Nexo. ¡Pronto serás redireccionado al repositorio oficial!");
+    setTimeout(() => {
+        window.location.href = "https://github.com/NexoProyect/NexoProyect/blob/main/README.md";
+    }, 3000);
 });
